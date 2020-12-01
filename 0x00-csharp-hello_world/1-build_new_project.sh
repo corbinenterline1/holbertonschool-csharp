73fb7a5c987f144a91d54b43aebaeaf05ab8b34b
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-01 02:25:07 +0000

    1

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Initializes & builds a new C# project
-dotnet new console -o 1-new_project
+dotnet new console -n 1-new_project
 dotnet build 1-new_project/1-new_project.csproj
