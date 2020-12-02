67d11b7d69e5082ea85096359e5270298edb39cd
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-01 19:41:40 -0600

    3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char iterletter = 'a'; iterletter <= 'z'; iterletter++)
+            if (iterletter != 'q' && iterletter != 'e')
+                Console.Write(iterletter);
+    }
+}
