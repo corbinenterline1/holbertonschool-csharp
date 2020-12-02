7fc69a20768d2a2708818f8c7ab42148a462864e
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-01 20:06:55 -0600

    6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tenz = 0;
+        int onez = 1;
+        while (tenz < 9)
+        {
+            while (onez <= 9 && tenz != 8)
+            {  
+                if (onez != tenz)
+                    Console.Write("{0}{1}, ", tenz, onez);
+                onez++;
+            }
+            tenz++;
+            onez = tenz;
+        }
+        Console.WriteLine("{0}{1}", tenz - 1, onez);
+    }
+}
