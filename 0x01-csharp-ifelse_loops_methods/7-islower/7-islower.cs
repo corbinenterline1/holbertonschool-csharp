2fa74372cd66043569516c3a22cd6f0d56b59209
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-01 20:23:37 -0600

    7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (((char)c > 96) && ((char)c < 123))
+            return true;
+        else
+            return false;
+    }
+}
