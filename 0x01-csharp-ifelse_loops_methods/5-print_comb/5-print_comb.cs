44f311c16aff3ed587c80edab44721496a697ff0
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-01 19:51:52 -0600

    5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+            {
+                if (i < 99)
+                    Console.Write("{0:D2}, ", i);
+                else
+                    Console.Write("{0:D2}\n", i);
+            }
+    }
+}
