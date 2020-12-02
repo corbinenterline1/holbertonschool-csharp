5f5dc2cfc0bbad239f25016b0e1a1cf07615d6e2
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-01 21:18:08 -0600

    12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        while (i < 101)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                Console.Write("FizzBuzz");
+            else if (i % 3 == 0)
+                Console.Write("Fizz");
+            else if (i % 5 == 0)
+                Console.Write("Buzz");
+            else
+                Console.Write("{0}", i);
+            if (i < 100)
+                Console.Write(" ");
+            else
+                Console.Write("\n");
+            i++;
+        }
+    }
+}
