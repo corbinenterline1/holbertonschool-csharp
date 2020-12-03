72d450668584d8fec304a2f6553f17e3b303fb83
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 20:04:30 -0600

    3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        int i = array.Length - 1;
+        for (; i > 0; i--)
+        {
+            Console.Write("{0} ", array[i]);
+        }
+        Console.WriteLine("{0}", array[i]);
+    }
+}
