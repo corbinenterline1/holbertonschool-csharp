5eacfdc98e5745bbf55d4aee5f0a55de16aad1f5
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 21:49:49 -0600

    14

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -5,19 +5,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] arr2d = new int[5, 5]{
-                                        {0, 0, 0, 0, 0},
-                                        {0, 0, 0, 0, 0},
-                                        {0, 0, 1, 0, 0},
-                                        {0, 0, 0, 0, 0},
-                                        {0, 0, 0, 0, 0}
-
-                                    };
-        int row_l = arr2d.GetLength(0);
-        int col_l = arr2d.GetLength(1);
-        for (int row = 0; row  < row_l; row++)
+        int[,] arr2d = new int[5, 5];
+        for (int row = 0; row  < 5; row++)
         {
-            for (int col = 0; col < col_l; col++)
+            for (int col = 0; col < 5; col++)
                 Console.Write(string.Format("{0} ", arr2d[row, col]));
             Console.WriteLine();
         }
