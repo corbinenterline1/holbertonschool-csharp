026a09b16fe244c9e014d53ee29de3d57de1ab5e
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 21:17:49 -0600

    11

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> two_dic = new Dictionary<string, int> ();
+        foreach(KeyValuePair<string, int> combo in myDict)
+            two_dic[combo.Key] = combo.Value * 2;
+        return two_dic;
+    }
+}
