8e2ebc2d15d46f0bc7a8f1f54c70c0a185569496
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 22:31:34 -0600

    15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int row_l = myMatrix.GetLength(0);
+        int col_l = myMatrix.GetLength(1);
+        int[,] arr2d = new int[row_l, col_l];
+        for (int i = 0; i < row_l; i++)
+        {
+            for (int j = 0; j < col_l; j++)
+                arr2d[i, j] = myMatrix[i, j] * myMatrix[i, j];
+        }
+        return arr2d;
+    }
+}
