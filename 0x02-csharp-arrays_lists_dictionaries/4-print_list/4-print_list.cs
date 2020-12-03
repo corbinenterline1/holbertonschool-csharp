e9ff13416605d3dd67a9047eb27948ff885235cf
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 20:20:10 -0600

    4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            List<int> print_list = new List<int>();
+            if (size == 0)
+                    Console.WriteLine();
+            else
+            {
+                for (int i = 0; i <= size - 1; i++)
+                {
+                        print_list.Add(i);
+                        if (i != size - 1)
+                            Console.Write("{0} ", i);
+                        else
+                            Console.WriteLine("{0}", i);
+                }
+            }
+            return print_list;
+        }
+    }
+}
