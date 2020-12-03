c963dc8b1504e7a662ed3fb82bd887e09e3a8a7f
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 21:25:39 -0600

    12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var glory_list = myDict.Keys.ToList();
+        glory_list.Sort();
+        foreach (var key in glory_list)
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+    }
+}
