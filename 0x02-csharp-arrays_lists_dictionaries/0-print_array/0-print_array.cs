872c515ba2d0c4ac8106280872c71de73220e976
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 19:41:53 -0600

    0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            int[] new_array = new int[size];
+            if (size == 0)
+                Console.WriteLine();
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    new_array[i] = i;
+                    if (i == size - 1)
+                        Console.WriteLine("{0}", i);
+                    else
+                        Console.Write("{0} ", i);
+                }
+            }
+            return new_array;
+        }
+    }
+}
