e913dd3b3d96dedd19d488632ad26822f9d3531f
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-02 21:32:58 -0600

    13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max_key = 0;
+        string record_keeper = "None";
+        foreach(KeyValuePair<string, int> combo in myList)
+        {
+            if (combo.Value > max_key)
+            {
+                max_key = combo.Value;
+                record_keeper = combo.Key;
+            }
+        }
+        return record_keeper;
+    }
+}
