cc30739e51455c1f3f818422307196dc22cbe5dc
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 22:07:49 -0600

    13 first pass

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (myLList.Count == 0)
+            return;
+        var nodey = myLList.First;
+        for (int i = 0; i <= index && nodey != null; i++)
+        {
+            if (i != index)
+            {
+                nodey = nodey.Next;
+                continue;
+            }
+            myLList.Remove(nodey);
+            return;
+        }
+    }
+}
