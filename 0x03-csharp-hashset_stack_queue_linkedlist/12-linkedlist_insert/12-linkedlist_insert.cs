b01872953c38a350c1bfdf1097b7e4256ae21cd4
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 21:58:39 -0600

    12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0)
+        {
+            myLList.AddFirst(n);
+            return myLList.First;
+        }
+        var nodey = myLList.First;
+        for (; nodey.Next != null; nodey = nodey.Next)
+        {
+            if (n <= nodey.Value)
+            {
+                myLList.AddBefore(nodey, n);
+                return nodey.Previous;
+            }
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
+    }
+}
