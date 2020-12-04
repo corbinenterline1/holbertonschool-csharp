8c82f076014dd536ec573922df5fe5117dd0eaba
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 20:52:26 -0600

    5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> ll_cool_j = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            ll_cool_j.AddLast(i);
+            Console.WriteLine(ll_cool_j.Last.Value);
+        }
+        return ll_cool_j;
+    }
+}
