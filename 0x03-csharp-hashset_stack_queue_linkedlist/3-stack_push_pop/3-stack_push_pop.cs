0b95a3b61e604bb345e07ff0f0da704a9c8e3064
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 20:34:22 -0600

    3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        bool forgot_about_bool = aStack.Contains(search);
+        Console.WriteLine("Stack contains {0}: {1}", search, forgot_about_bool);
+        while(aStack.Contains(search))
+        {
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
