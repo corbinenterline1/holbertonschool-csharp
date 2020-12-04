2e5c788edbd5012ecc3eb11e4c6226d02d083434
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 21:35:17 -0600

    10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        var nodey = myLList.First;
+        for (int i = 0; i < myLList.Count && nodey != null; i++, nodey = nodey.Next)
+        {
+            if (i == n)
+                return nodey.Value;
+        }
+        return 0;
+    }
+}
