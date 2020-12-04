1c02c9565935b8d0a5ace7185124f9d42a473e8e
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 19:39:07 -0600

    0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int total = 0;
+        HashSet<int> hash_it_out = new HashSet<int>(myList);
+        foreach (int i in hash_it_out)
+            total += i;
+        return total;
+    }
+}
