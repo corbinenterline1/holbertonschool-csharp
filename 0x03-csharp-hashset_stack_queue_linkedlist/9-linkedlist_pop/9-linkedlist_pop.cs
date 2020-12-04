8cd552e0d9cdfc02e6d8854d5378c081eb7e6890
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 21:21:19 -0600

    9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int nodey = myLList.First.Value;
+        myLList.RemoveFirst();
+        return nodey;
+    }
+}
