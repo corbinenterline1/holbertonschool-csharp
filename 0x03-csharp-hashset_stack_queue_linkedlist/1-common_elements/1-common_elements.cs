9314ede36e1ae30763369d3ff7290b71c52a5667
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 19:49:47 -0600

    1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> common_list = new List<int>();
+        for (int i = 0; i < list2.Count; i++)
+        {
+            if (list1.Contains(list2[i]))
+                common_list.Add(list2[i]);
+        }
+        common_list.Sort();
+        return common_list;
+    }
+}
