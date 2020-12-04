c5bf2ca389035b034bf7a9bbd60d502618ba5185
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 21:40:49 -0600

    11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int total = 0;
+        var nodey = myLList.First;
+        for (; nodey != null; nodey = nodey.Next)
+            total += nodey.Value;
+        return total;
+    }
+}
