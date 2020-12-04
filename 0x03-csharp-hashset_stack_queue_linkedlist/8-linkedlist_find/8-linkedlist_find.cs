c7cc48cfa37877f6614b2b876fed7169f3326f1c
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-03 21:13:59 -0600

    8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        var nodey = myLList.First;
+        for (int i = 0; nodey != null; i++, nodey = nodey.Next)
+        {
+            if (nodey.Value == value)
+                return i;
+        }
+        return -1;
+    }
+}
