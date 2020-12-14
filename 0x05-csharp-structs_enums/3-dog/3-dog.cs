8baa247ae233e1f77058b189f6df8d4f09df589f
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-14 13:51:56 -0600

    dogs

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace _3_dog
+enum Rating
 {
-    class Program
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
     }
+
+    public override string ToString()
+    {
+        return String.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+    }
+
 }
