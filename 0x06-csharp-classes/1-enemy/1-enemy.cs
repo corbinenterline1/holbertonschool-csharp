5307b2445757e4e2d39704981dadc7bb661792db
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-15 12:51:01 -0600

    1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a brain-eating ghoul.
+    /// </summary>
+    public class Zombie 
+    {
+        /// <summary>
+        /// Represents the health of the Zombie object.
+        /// This public field is a member of the Zombie class.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
