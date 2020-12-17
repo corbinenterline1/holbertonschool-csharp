2d06e040d40e3f6bff95372fb81419aebdfa1721
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-17 17:53:05 -0600

    0

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class containing a few different algebraic operations.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Returns the sum of ints a and b.
+        /// </summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
