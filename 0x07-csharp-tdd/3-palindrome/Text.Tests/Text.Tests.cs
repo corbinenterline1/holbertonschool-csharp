bae1d1c120514a0a343e040f8a8df3910d50ca41
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-17 20:27:01 -0600

    3

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [Test]
+        public void PalTest()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome("level"));
+        }
+        [Test]
+        public void PalCaseTest()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome("Racecar"));
+        }
+        [Test]
+        public void PalSpaceAndStuffTest()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome("A man, a plan, a canal: Panama."));
+        }
+        [Test]
+        public void PalEmptyTest()
+        {
+            Assert.IsTrue(Text.Str.IsPalindrome(""));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+
+namespace Text
+{
+    /// <summary>
+    /// Str class
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Determines if a string is a palindrome
+        /// </summary>
+        public static bool IsPalindrome(string s)
+        {
+            string clean_s = s.ToLower();
+            clean_s = Regex.Replace(clean_s, "[^a-z]*", "");
+            char[] array = clean_s.ToCharArray();
+            Array.Reverse(array);
+            string rev_clean_s = new string(array);
+            return clean_s == rev_clean_s;
+        }
+    }
+}
