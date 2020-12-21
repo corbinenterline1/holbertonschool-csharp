78b8e44175373aefc472da1fd893cd35b2f47a8f
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-21 12:50:33 -0600

    2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Custom Object class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if object is an instance of a class that inherits from the specified class.
+    /// Must be a subclass, not an instance of the base class. Returns true or false.
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}
