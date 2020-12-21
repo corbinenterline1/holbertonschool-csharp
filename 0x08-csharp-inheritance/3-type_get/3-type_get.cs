59ac32d9f3acc2892128ef7d4de116ee8d76ebbc
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-21 13:00:41 -0600

    3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Custom Object class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method that prints the names of the available properties and methods of an object, format shown in example.
+    /// </summary>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        foreach(var iprop in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(iprop.Name);
+        }
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach(var imeth in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(imeth.Name);
+        }
+    }
+}
