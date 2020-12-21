27d47eaa20a3a2cd11dcc3cd72236796eca3f32a
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-21 13:10:42 -0600

    5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+/// <summary>
+/// Shape class. Shapely stuff.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// For now it throws a NotImplementedException with message.
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
