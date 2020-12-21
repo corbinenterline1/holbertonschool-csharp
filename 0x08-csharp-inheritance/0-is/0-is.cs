f93b30e8eb2eb86288dcfa3da8aa118e1c7446f0
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-21 12:38:53 -0600

    0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Object class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if object is an Int.
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+        {
+            return true;
+        }
+        return false;
+    }
+}
