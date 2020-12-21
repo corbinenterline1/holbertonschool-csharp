ef6b1642544609a8a1754d0846e87b8f4b444fd2
Corbin Enterline <corbinenterline1@gmail.com>
2020-12-21 12:46:42 -0600

    1 XML

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 
+/// <summary>
+/// Custom Object class.
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// Checks if object is an instance of, or an instance of a class
+    /// that inherited from Array.
+    /// </summary>
     public static bool IsInstanceOfArray(object obj)
     {
         if (obj is Array)
