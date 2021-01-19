7d21336e96601f0ab3aa89c8353c2404d98cd632
Corbin Enterline <corbinenterline1@gmail.com>
2021-01-19 13:21:19 -0600

    6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -9,14 +9,26 @@ class VectorMath
         /// Add mushes* vectors together. *technical term
         /// </summary>
         public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == 2 && vector2.Length == 2)
         {
-            if (vector1.Length == 2 && vector2.Length == 2)
-            {
-                return (new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1]});
-            }
-            if (vector1.Length == 3 && vector2.Length == 3)
-            {
-                return (new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]});
-            }
-            return (new double[] {-1});
+            double[] retvect = new double[] {0, 0};
+            retvect[0] = Math.Round((vector1[0] + vector2[0]), 2);
+            retvect[1] = Math.Round((vector1[1] + vector2[1]), 2);
+            return (retvect);
         }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] retvect = new double[] {0, 0, 0};
+            retvect[0] = Math.Round((vector1[0] + vector2[0]), 2);
+            retvect[1] = Math.Round((vector1[1] + vector2[1]), 2);
+            retvect[2] = Math.Round((vector1[2] + vector2[2]), 2);
+            return (retvect);
+        }
+        else
+        {
+            double[] retvect = new double[] {-1};
+            return (retvect);
+        }
+    }
+}
