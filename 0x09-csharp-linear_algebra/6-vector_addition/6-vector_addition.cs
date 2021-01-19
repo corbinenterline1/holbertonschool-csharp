de29a2e09aa1c3e6a55a6b8c8742ed849e5fb756
Corbin Enterline <corbinenterline1@gmail.com>
2021-01-19 13:11:31 -0600

    do-over

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,28 +1,22 @@
 ﻿using System;
 
 /// <summary>
-/// Class VectorMath for Vector Math ;)
-// </summary>
+/// Class VectorMath for Vector Math
+/// </summary>
 class VectorMath
-{
-    public static double[] Add(double[] vector1, double[] vector2)
     {
-        if (vector1.Length == 2 && vector2.Length == 2)
+        /// <summary>
+        /// Add mushes* vectors together. *technical term
+        /// </summary>
+        public static double[] Add(double[] vector1, double[] vector2)
         {
-            double[] retvect = new double[] {0, 0};
-            retvect[0] = Math.Round((vector1[0] + vector2[0]), 2);
-            retvect[1] = Math.Round((vector1[1] + vector2[1]), 2);
-            return (retvect);
+            if (vector1.Length == 2 && vector2.Length == 2)
+            {
+                return (new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1]});
+            }
+            if (vector1.Length == 3 && vector2.Length == 3)
+            {
+                return (new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]});
+            }
+            return (new double[] {-1});
         }
-        if (vector1.Length == 3 && vector2.Length == 3)
-        {
-            double[] retvect = new double[] {0, 0, 0};
-            retvect[0] = Math.Round((vector1[0] + vector2[0]), 2);
-            retvect[1] = Math.Round((vector1[1] + vector2[1]), 2);
-            retvect[2] = Math.Round((vector1[2] + vector2[2]), 2);
-            return (retvect);
-        }
-        double[] retvect = new double[] {-1};
-        return (retvect);
-    }
-}
