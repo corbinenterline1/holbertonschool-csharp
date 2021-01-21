a77cbd3862f7030f0aa307c0ec39b29c8387ebbe
Corbin Enterline <corbinenterline1@gmail.com>
2021-01-21 14:11:51 -0600

    smh

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -13,16 +13,16 @@ class VectorMath
         if (vector.Length == 2)
         {
             double[] retvect = new double[] {0, 0};
-            retvect[0] = Math.Round((vector1[0] * scalar), 2);
-            retvect[1] = Math.Round((vector1[1] * scalar), 2);
+            retvect[0] = Math.Round((vector[0] * scalar), 2);
+            retvect[1] = Math.Round((vector[1] * scalar), 2);
             return (retvect);
         }
         else if (vector.Length == 3)
         {
             double[] retvect = new double[] {0, 0, 0};
-            retvect[0] = Math.Round((vector1[0] * scalar), 2);
-            retvect[1] = Math.Round((vector1[1] * scalar), 2);
-            retvect[2] = Math.Round((vector1[2] * scalar), 2);
+            retvect[0] = Math.Round((vector[0] * scalar), 2);
+            retvect[1] = Math.Round((vector[1] * scalar), 2);
+            retvect[2] = Math.Round((vector[2] * scalar), 2);
             return (retvect);
         }
         else
