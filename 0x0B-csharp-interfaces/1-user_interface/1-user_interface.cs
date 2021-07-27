2f3e071c884151cd8a437df0b50a06d9769c2e5e
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-27 13:50:21 -0500

    1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>Abstract base class.</summary>
+public abstract class Base
+{
+    public string name {get; set;}
+    
+    /// <summary>ToString override.</summary>
+    public override string ToString()
+    {
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
+    }
+}
