332910aec96c2d04c504c9339a41f0aeb791ef82
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-27 09:09:58 -0500

    print dat queue

---
+++ 0x0A-csharp-generics/4-print/queue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+/// <summary>Queue class.</summary>
+class Queue<T>
+{
+    /// <summary>Node class contained in queue.</summary>
+    public class Node
+    {
+        public T value;
+        public Node next = null;
+
+        /// <summary>Node constructor.</summary>
+        public Node(T val)
+        {
+            value = val;
+        }
+    }
+
+    /// <summary>Head node of queue.</summary>
+    public Node head = null;
+    /// <summary>Tail node of queue.</summary>
+    public Node tail = null;
+    /// <summary>Count of nodes.</summary>
+    public int count = 0;
+
+    /// <summary>Method that creates a new Node and adds it to the end of the queue.</summary>
+    public void Enqueue(T val){
+        Node n = new Node(val);
+        
+        if (head == null){
+            head = n;
+        }
+        if (tail != null)
+            tail.next = n;
+        else
+            head.next = n;
+        tail = n;
+        count++;
+    }
+
+    /// <summary>Returns the value of the first node of the queue without removing the node.</summary>
+    public T Peek()
+    {
+        if (head == null || count == 0){
+            Console.WriteLine("Queue is empty");
+            return (default(T));
+        }
+        return (head.value);
+    }
+
+    /// <summary>Method that prints the queue, starting from the head.</summary>
+    public void Print()
+    {
+        Node p = head;
+        if (head == null || count == 0){
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+        for (int i = 0; i < count; i++){
+            Console.WriteLine(p.value);
+            p = p.next;
+        }
+    }
+    /// <summary>Method that removes the first node in the queue and returns its' value.</summary>
+    public T Dequeue(){
+        Node n;
+
+        if (head == null || count == 0){
+            Console.WriteLine("Queue is empty");
+            return (default(T));
+        }
+        else
+        {
+            Node h = head;
+            n = head.next;
+            head = n;
+            count--;
+            return h.value;
+        }
+    }
+    /// <summary>Returns the Queue's type.</summary>
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+
+    /// <summary>Counts the length of the queue and returns it.</summary>
+    public int Count()
+    {
+        return (count);
+    }
+}
