41e54701b7be3c45ad8f288616fc1540d6d417c0
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-27 15:02:15 -0500

    foreach classes ya goof

---
+++ 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>Abstract base class.</summary>
+public abstract class Base
+{
+    ///<summary>Base class name.</summary>
+    public string name {get; set;}
+
+    /// <summary>ToString override.</summary>
+    public override string ToString()
+    {
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
+    }
+}
+
+/// <summary>Interactive interface.</summary>
+public interface IInteractive {
+    /// <summary>Interact method.</summary>
+    void Interact();
+}
+
+/// <summary>Breakable interface.</summary>
+public interface IBreakable {
+    ///<summary>Durability.</summary>
+    int durability {get; set;}
+
+    ///<summary>Break.</summary>
+    void Break();
+}
+
+/// <summary>Collectable interface.</summary>
+public interface ICollectable {
+    ///<summary>Property showing whether an object is collected.</summary>
+    bool isCollected {get; set;}
+
+    ///<summary>Collect method.</summary>
+    void Collect();
+}
+
+///<summary>Door class that inherits from Base and IInteractive.</summary>
+public class Door : Base, IInteractive
+{
+    ///<summary>Door constructor.</summary>
+    public Door(string d="Door")
+    {
+        this.name = d;
+    }
+
+    ///<summary>Interact method.</summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0}. It's locked.", this.name);
+    }
+}
+
+///<summary>Decoration class that inherits from Base, Interactive, and Breakable.</summary>
+public class Decoration : Base, IInteractive, IBreakable
+{
+    ///<summary>Is it a quest item?</summary>
+    public bool isQuestItem;
+
+    ///<summary>Durability.</summary>
+    public int durability {get; set; }
+
+    ///<summary>Decoration constructor.</summary>
+    public Decoration(string name="Decoration", int durability=1, bool isQuestItem=false)
+    {
+        if (durability <= 0)
+            throw new Exception("Durability must be greater than 0");
+        this.name = name;
+        this.isQuestItem = isQuestItem;
+        this.durability = durability;
+    }
+    
+    ///<summary>Interact implementation.</summary>
+    public void Interact()
+    {
+        if (durability <= 0)
+            Console.WriteLine("The {0} has been broken.", name);
+        else if (isQuestItem == true)
+            Console.WriteLine("You look at the {0}. There's a key inside.", name);
+        else
+            Console.WriteLine("You look at the {0}. Not much to see here.", name);
+    }
+
+    ///<summary>Break implementation.</summary>
+    public void Break()
+    {
+        durability--;
+        if (durability > 0)
+            Console.WriteLine("You hit the {0}. It cracks.", name);
+        else if (durability == 0)
+            Console.WriteLine("You smash the {0}. What a mess.", name);
+        else
+            Console.WriteLine("The {0} is already broken.", name);
+    }
+}
+
+///<summary>Key class that inherits from Base and ICollectable.</summary>
+public class Key : Base, ICollectable
+{
+    ///<summary>Property showing whether an object is collected.</summary>
+    public bool isCollected {get; set;}
+
+    ///<summary>Key constructor.</summary>
+    public Key(string name="Key", bool isCollected=false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    ///<summary>Key collect!</summary>
+    public void Collect()
+    {
+        if (isCollected == false)
+        {
+            isCollected = true;
+            Console.WriteLine("You pick up the {0}.", name);
+        }
+        else
+            Console.WriteLine("You have already picked up the {0}.", name);
+    }
+}
+///<summary>Generic class Objs that creates a collection of type T objects that can be iterated through using foreach.</summary>
+class Objs<T> : IEnumerable<T>
+{
+    List<T> tobjects = new List<T>();
+
+    public void Add(T obj)
+    {
+        tobjects.Add(obj);
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        return tobjects.GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return (this.GetEnumerator());
+    }
+}
