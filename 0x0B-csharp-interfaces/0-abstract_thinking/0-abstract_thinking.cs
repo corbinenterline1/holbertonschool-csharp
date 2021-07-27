0027f3b7821f2b67630c6d63d9d2d33cd2a70886
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-27 12:44:49 -0500

    abstract base class & comments

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-class Base
+
+/// <summary>Abstract base class.</summary>
+public abstract class Base
 {
     public string name {get; set;}
 
+    /// <summary>ToString override.</summary>
     public override string ToString()
     {
         return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
