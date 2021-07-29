49f111a452e813bde7f9078be1e5f04df7e8eaed
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-29 10:28:06 -0500

    0 & 1

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+///<summary>Player class.</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Player class constructor.</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    ///<summary>Prints health of player.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+}
---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+///<summary>Player class.</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Player class constructor.</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    ///<summary>Prints health of player.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+
+    ///<summary>Method to calculate Player's damage taken.</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+            damage = 0f;
+        Console.WriteLine("{0} takes {1} damage!", name, damage);
+    }
+
+    ///<summary>Method to calculate Player's health restored.</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0f)
+            heal = 0f;
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+    }
+}
+
+///<summary>Delegate to calculate Player's health.</summary>
+public delegate void CalculateHealth(float damage);
