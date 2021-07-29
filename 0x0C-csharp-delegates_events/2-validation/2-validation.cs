9f470ef8a1ec93dd7c552c3e91f32438d4f35fc7
Corbin Enterline <corbinenterline1@gmail.com>
2021-07-29 10:43:31 -0500

    2

---
+++ 0x0C-csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+///<summary>Player class.</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Player class constructor.</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    ///<summary>Prints health of player.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+
+    ///<summary>Method to calculate Player's damage taken.</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+            damage = 0f;
+        float newHp = hp - damage;
+        ValidateHP(newHp);
+        Console.WriteLine("{0} takes {1} damage!", name, damage);
+    }
+
+    ///<summary>Method to calculate Player's health restored.</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0f)
+            heal = 0f;
+        float newHp = hp + heal;
+        ValidateHP(newHp);
+        Console.WriteLine("{0} heals {1} HP!", name, heal);
+    }
+
+    ///<summary>Method to validate the HP of the player.  HP can't be below 0 or above maxHp.</summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp <= 0)
+            hp = 0;
+        else if (newHp >= maxHp)
+            hp = maxHp;
+        else
+            hp = newHp;
+    }
+}
+
+///<summary>Delegate to calculate Player's health.</summary>
+public delegate void CalculateHealth(float damage);
